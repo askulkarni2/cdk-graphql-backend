023d43fc2a0465a2d8c7c55ee1a82c0f40ffccbe
Apoorva Kulkarni <kuapoorv@amazon.com>
2020-09-24 23:38:47 -0700

    adding Pipeline module to wrap things in a bow

--- bin/appsync-cdk-app.ts
+++ bin/appsync-cdk-app.ts
@@ -1,7 +1,62 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
 import { AppsyncCdkAppStack } from '../lib/appsync-cdk-app-stack';
+import { App, Construct, Stage, Stack, StackProps, StageProps, SecretValue } from '@aws-cdk/core';
+import { CdkPipeline, SimpleSynthAction } from '@aws-cdk/pipelines';
+import * as codepipeline from '@aws-cdk/aws-codepipeline';
+import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
 
-const app = new cdk.App();
-new AppsyncCdkAppStack(app, 'AppsyncCdkAppStack');
+/**
+ * Your application
+ *
+ * May consist of one or more Stacks (here, two)
+ *
+ * By declaring our DatabaseStack and our ComputeStack inside a Stage,
+ * we make sure they are deployed together, or not at all.
+ */
+class AppSyncApplication extends Stage {
+    constructor(scope: Construct, id: string, props?: StageProps) {
+        super(scope, id, props);
+
+        // create the application stack here
+        new AppsyncCdkAppStack(this, 'AppsyncCdkAppStack');
+    }
+}
+
+/**
+ * Stack to hold the pipeline
+ */
+class PipelineStack extends Stack {
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
+
+        const sourceArtifact = new codepipeline.Artifact();
+        const cloudAssemblyArtifact = new codepipeline.Artifact();
+
+        const pipeline = new CdkPipeline(this, 'Pipeline', {
+            pipelineName: 'AppSyncPipeline',
+            cloudAssemblyArtifact,
+
+            sourceAction: new codepipeline_actions.GitHubSourceAction({
+                actionName: 'GitHub',
+                output: sourceArtifact,
+                oauthToken: SecretValue.secretsManager('GITHUB_TOKEN'),
+                // Replace these with your actual GitHub project name
+                owner: 'askulkarni2',
+                repo: 'cdk-graphql-backend',
+            }),
+
+            synthAction: SimpleSynthAction.standardNpmSynth({
+                sourceArtifact,
+                cloudAssemblyArtifact,
+
+                // Use this if you need a build step (if you're not using ts-node
+                // or if you have TypeScript Lambdas that need to be compiled).
+                buildCommand: 'npm run build',
+            }),
+        });
+    }
+}
+
+const app = new App();
+new PipelineStack(app, 'PipelineStack');
